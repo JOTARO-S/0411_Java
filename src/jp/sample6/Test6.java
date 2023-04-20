1ebefb384d2b5d369d655e31ea6b0e980e2f3ed5
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-04-20 15:32:50 +0900

    Sample6

---
+++ src/jp/sample6/Test6.java
@@ -0,0 +1,238 @@
+package jp.sample6;
+
+/* 6-1
+ * 
+ *  A
+ * 
+ * 
+ */
+
+/* public class Test6 {
+	public static void main(String args) {
+		byte x = 5;
+		byte y = 6 * x++;
+		byte z = x * y;
+		System.out.println(" z : " + z);
+	}
+}
+
+*/
+
+/* 6-2
+ * 
+ *  E
+ * 
+ * 
+ */
+
+/* 6-3
+ * 
+ *  D
+ * 
+ */
+
+/* 6-4
+ * 
+ * A, C, G
+ * 
+ * 
+ */
+/*
+
+class A {
+	void method() {}
+	void method2() {}
+	void method3() {}
+	void method4() {}
+	void method5() {}
+	void method6() {}
+	void method7() {}
+}
+
+class B extends A {
+	//public method() {}
+	public void method2() {}
+	//public int method3() {return 0;}
+	void method4(int i, String s) {}
+	void method5(Object o) {}
+	protected void method6() {}
+	//private void method7() {}
+	
+}
+
+*/
+
+/* 6-5
+ * 
+ * B
+ * 
+ */
+
+/* 6-6
+ * 
+ *  F
+ * 
+ */
+
+/*
+
+public class Test6 {
+	private String msg;
+	public Test6() { this ("Test Constructor"); }
+	public Test6(String msg) { msg = msg ;}
+	public String toString() { return ("msg : " + msg); }
+	
+	public static void main(String[] args) {
+		System.out.println(new Test6());
+	}
+}
+
+*/
+
+/* 6-7
+ * 
+ *  D
+ * 
+ * 
+ */
+
+/* 6-8
+ * 
+ * 
+ *  B
+ * 
+ * 
+ */
+
+/*
+abstract class AbsSuper {
+	public abstract void show();
+}
+
+class Sub extends AbsSuper {
+	void show() { //可視化されてない
+		System.out.println("^^;");
+	}
+}
+
+public class Test6 extends Sub {
+	public static void main(String[] args) {
+		new Test6().show();
+	}
+}
+
+*/
+
+/* 6-9
+ * 
+ *  D
+ * 
+ */
+
+/* 6-10
+ * 
+ * A, F
+ * 
+ * 
+ */
+/*
+
+interface X {}
+interface Y {}
+
+//interface Z extends X, Y {}
+class Z implements X, Y {}
+
+*/
+
+/* 6-11
+ * 
+ * B, D
+ * 
+ */
+
+/*
+
+interface AA { public int m1(); }
+interface BB { void m2();  }
+
+class Test implements AA, BB {
+	public int m1() { return 0; }
+	public void m2() {}
+}
+
+abstract class Test2 implements AA, BB {
+	public int m1() {return 0; }
+}
+
+*/
+
+/* 6-12
+ * 
+ *  B
+ * 
+ */
+
+/* 6-13
+ * 
+ *  A, E
+ * 
+ * 
+ */
+
+/* 6-14
+ * 
+ *  A, C
+ * 
+ * 
+ */
+
+/* 6-15
+ * 
+ *  C
+ * 
+ */
+
+/* 6-16
+ * 
+ * D
+ * 
+ * 
+ */
+
+/* 6-17
+ * 
+ * B
+ * 
+ */
+
+/* 6-18
+ * 
+ *  B
+ * 
+ */
+
+/* 6-19
+ * 
+ * B
+ * 
+ */
+
+/* 6-20
+ * 
+ *  D
+ * 
+ */
+
+/* 6-21
+ * 
+ * D
+ * 
+ */
+
+/* 6-22
+ * 
+ *  C
+ * 
+ */
+
