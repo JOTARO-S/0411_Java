2196b76d88a96b5d312f2a052ee01299f0a1e067
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-04-26 17:32:42 +0900

    JDBC Sample

--- src/jp/Jdbc/Sample2.java
+++ src/jp/Jdbc/Sample2.java
@@ -20,7 +20,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Statement; //Ctrl + shift + O でインポートする際に java.sql(sqlの場合)を選択する(複数の候補が出てくるため)
 
 public class Sample2 {
 	public static void main(String[] args) {
